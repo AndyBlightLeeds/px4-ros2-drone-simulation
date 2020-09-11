0ebf1501e90635120a11b6ecf7f8b475543c0cfb
Andy Blight <a.j.blight@leeds.ac.uk>
2020-09-11 16:51:28 +0100

    Fixed spawn_model.bash

--- scripts/run/spawn_model.bash
+++ scripts/run/spawn_model.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
+# Setup environment,
 . ./params.bash
-. ~/gazebo_learning_ws/install/setup.bash
+export PX4_SITL_GAZEBO_GIT_DIR
+. ${COLCON_WS_DIR}/install/setup.bash
 
+# Spawn model.
 ./spawn_model.py
