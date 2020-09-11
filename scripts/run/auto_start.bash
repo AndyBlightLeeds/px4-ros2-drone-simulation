f8501c741e1423d06bcd47adc4ad4353de79dc34
Andy Blight <a.j.blight@leeds.ac.uk>
2020-09-11 16:56:42 +0100

    Working on auto_start.bash.  PX4 exits early.

--- scripts/run/auto_start.bash
+++ scripts/run/auto_start.bash
@@ -6,25 +6,29 @@ set -e
 . ./params.bash
 
 # There is a `sleep 1` between each command to allow it time to finish.
-# This is needed as
+# This is needed as each process takes a while to get started.
+
 # Gazebo server and client.
-./gzserver.bash &
+./gzserver.bash &> gzserver.log &
 sleep 1
-./gzclient.bash &
+./gzclient.bash &> gzclient.log &
 sleep 1
 
 # Spawn the model. This terminates after being run.
-./spawn_model.bash
+./spawn_model.bash &> spawn_model.log
 
 # Start PX4.
 # TODO It would be good to keep this session interactive as I have
 # found it useful to be able to run PX4 commands when debugging.
-./px4.bash &
+./px4.bash &> px4.log &
+sleep 1
 # Start microRTPS client on PX4. This terminates after being run.
-./micrortps_client.bash
+./micrortps_client.bash &> gzserver.log &
+sleep 1
 
 # uRTPS agent.
-./micrortps_agent.bash &
+./micrortps_agent.bash &> gzserver.log &
+sleep 1
 
 echo "Done"
 
